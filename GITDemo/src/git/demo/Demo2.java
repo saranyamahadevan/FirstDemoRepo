7f7aee070235071e605ca4a937076f24568daeaf
saranya <mahadevanandhi.s@gmail.com>
2022-12-20 12:45:24 +0800

    Initial setup

---
+++ GITDemo/src/git/demo/Demo2.java
@@ -0,0 +1,15 @@
+package git.demo;
+
+public class Demo {
+
+	public static void main(String[] args)
+	{
+	
+	System.out.println("My first Git");
+<<<<<<< HEAD
+	
+		System.out.println("My first Git-333333333");
+=======
+>>>>>>> parent of 3bc5d99 (Update Demo.java)
+	}
+}
